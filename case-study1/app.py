d0b9c1ad0503b8db2307a1f6d39cc3ceab05910d
bisharadi23-meet <109900012+bisharadi23-meet@users.noreply.github.com>
2022-08-05 11:12:50 +0300

    files added

---
+++ case-study1/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import session as login_session
+import pyrebase
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
+#code here
+
+
+#### dont code here
+if __name__ == '__main__':
+    app.run(debug=True)
