7c802640ec2cb88efc4fbe0af0d4709fa4f09690
bisharadi23-meet <109900012+bisharadi23-meet@users.noreply.github.com>
2022-08-05 11:30:10 +0300

    Update app.py

--- case-study1/app.py
+++ case-study1/app.py
@@ -5,6 +5,19 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 #code here
+config= {
+  "apiKey": "AIzaSyBtczwHSU6cZFPprkbV2KH-nolLCwx0q5g",
+  "authDomain": "case-study-664d7.firebaseapp.com",
+  "projectId": "case-study-664d7",
+  "storageBucket": "case-study-664d7.appspot.com",
+  "messagingSenderId": "360541122608",
+  "appId": "1:360541122608:web:fad092cf9abdf23e0ffdf5",
+  "measurementId": "G-SQ8GRL04S2"
+  "databaseURL": "https://case-study-664d7-default-rtdb.europe-west1.firebasedatabase.app/"}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
 
 
 #### dont code here
